d0fef7984ae9999ddb03b1b5b0a4bc6c1e5cbe58
Nick Pateman <nick@devoctomy.com>
2021-06-13 15:38:09 +0100

    More test coverage.

---
+++ devoctomy.Passchamp.Core.UnitTests/Graph/Cryptography/DeriveKeyFromPasswordNodeTests.cs
@@ -0,0 +1,49 @@
+﻿using devoctomy.Passchamp.Core.Graph;
+using devoctomy.Passchamp.Core.Graph.Cryptography;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace devoctomy.Passchamp.Core.UnitTests.Graph.Cryptography
+{
+    public class DeriveKeyFromPasswordNodeTests
+    {
+        [Fact]
+        public async Task GivenPassword_AndSalt_AndKeyLength_AndNextKey_WhenExecute_ThenKeyDerivedFromPassword_AndNextExecuted()
+        {
+            // Arrange
+            var password = "Hello World";
+            var salt = new byte[16];
+            var keyLength = 32;
+            var sut = new DeriveKeyFromPasswordNode
+            {
+                Password = new DataPin(password),
+                Salt = new DataPin(salt),
+                KeyLength = new DataPin(keyLength),
+                NextKey = "hello"
+            };
+            var mockGraph = new Mock<IGraph>();
+            var mockNextNode = new Mock<INode>();
+            mockGraph.Setup(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)))
+                .Returns(mockNextNode.Object);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            await sut.Execute(
+                mockGraph.Object,
+                cancellationTokenSource.Token);
+
+            // Assert
+            var keyBase64 = Convert.ToBase64String(sut.Key.GetValue<byte[]>());
+            Assert.Equal("4r6j4LSQOT/166ahn04oPny3pQxjnMu6sXCTBsi1z2Q=", keyBase64);
+            mockGraph.Verify(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)), Times.Once);
+            mockNextNode.Verify(x => x.Execute(
+                It.Is<IGraph>(x => x == mockGraph.Object),
+                It.Is<CancellationToken>(x => x == cancellationTokenSource.Token)), Times.Once);
+        }
+    }
+}
---
+++ devoctomy.Passchamp.Core.UnitTests/Graph/Cryptography/RandomByteArrayGeneratorNodeTests.cs
@@ -0,0 +1,51 @@
+﻿using devoctomy.Passchamp.Core.Graph;
+using devoctomy.Passchamp.Core.Graph.Cryptography;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace devoctomy.Passchamp.Core.UnitTests.Graph.Cryptography
+{
+    public class RandomByteArrayGeneratorNodeTests
+    {
+        [Fact]
+        public async Task GivenLength_AndNextKey_WhenExecute3Times_RandomBytesGenerated_AndAllUnique_AndNextExecuted()
+        {
+            // Arrange
+            var length = 32;
+            var sut = new RandomByteArrayGeneratorNode
+            {
+                Length = new DataPin(length),
+                NextKey = "hello"
+            };
+            var mockGraph = new Mock<IGraph>();
+            var mockNextNode = new Mock<INode>();
+            mockGraph.Setup(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)))
+                .Returns(mockNextNode.Object);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            var resultsList = new List<string>();
+            for(int i = 0; i < 3; i++)
+            {
+                await sut.Execute(
+                    mockGraph.Object,
+                    cancellationTokenSource.Token);
+                resultsList.Add(Convert.ToBase64String(sut.RandomBytes.GetValue<byte[]>()));
+            }
+
+            // Assert
+            Assert.Equal(3, resultsList.Distinct().ToList().Count);
+            mockGraph.Verify(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)), Times.Exactly(3));
+            mockNextNode.Verify(x => x.Execute(
+                It.Is<IGraph>(x => x == mockGraph.Object),
+                It.Is<CancellationToken>(x => x == cancellationTokenSource.Token)), Times.Exactly(3));
+        }
+    }
+}
