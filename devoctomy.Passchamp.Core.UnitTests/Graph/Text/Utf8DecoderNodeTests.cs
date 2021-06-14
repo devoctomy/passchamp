5b8674f61e987c35df5a360ae926fdcf2ac37cc8
Nick Pateman <nick@devoctomy.com>
2021-06-14 19:05:37 +0100

    More test coverage.

---
+++ devoctomy.Passchamp.Core.UnitTests/Graph/Text/Utf8DecoderNodeTests.cs
@@ -0,0 +1,43 @@
+﻿using devoctomy.Passchamp.Core.Graph;
+using devoctomy.Passchamp.Core.Graph.Text;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace devoctomy.Passchamp.Core.UnitTests.Graph.Text
+{
+    public class Utf8DecoderNodeTests
+    {
+        [Fact]
+        public async Task GivenEncodedBytes_WhenExecute_ThenPlainTextCorrect_AndNextExecuted()
+        {
+            // Arrange
+            var expectedPlainText = "Hello";
+            var sut = new Utf8DecoderNode
+            {
+                EncodedBytes = new Core.Graph.DataPin(new byte[] { 0x48, 0x65, 0x6c, 0x6c, 0x6f }),
+                NextKey = "hello"
+            };
+            var mockGraph = new Mock<IGraph>();
+            var mockNextNode = new Mock<INode>();
+            mockGraph.Setup(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)))
+                .Returns(mockNextNode.Object);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            await sut.Execute(
+                mockGraph.Object,
+                cancellationTokenSource.Token);
+
+            // Assert
+            Assert.Equal(expectedPlainText, sut.PlainText.GetValue<string>());
+            mockGraph.Verify(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)), Times.Once);
+            mockNextNode.Verify(x => x.Execute(
+                It.Is<IGraph>(x => x == mockGraph.Object),
+                It.Is<CancellationToken>(x => x == cancellationTokenSource.Token)), Times.Once);
+        }
+    }
+}
---
+++ devoctomy.Passchamp.Core.UnitTests/Graph/Text/Utf8EncoderNodeTests.cs
@@ -0,0 +1,43 @@
+﻿using devoctomy.Passchamp.Core.Graph;
+using devoctomy.Passchamp.Core.Graph.Text;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace devoctomy.Passchamp.Core.UnitTests.Graph.Text
+{
+    public class Utf8EncoderNodeTests
+    {
+        [Fact]
+        public async Task GivenEncodedBytes_WhenExecute_ThenPlainTextCorrect_AndNextExecuted()
+        {
+            // Arrange
+            var expectedEncodedBytes = new byte[] { 0x48, 0x65, 0x6c, 0x6c, 0x6f };
+            var sut = new Utf8EncoderNode
+            {
+                PlainText = new Core.Graph.DataPin("Hello"),
+                NextKey = "hello"
+            };
+            var mockGraph = new Mock<IGraph>();
+            var mockNextNode = new Mock<INode>();
+            mockGraph.Setup(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)))
+                .Returns(mockNextNode.Object);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            await sut.Execute(
+                mockGraph.Object,
+                cancellationTokenSource.Token);
+
+            // Assert
+            Assert.Equal(expectedEncodedBytes, sut.EncodedBytes.GetValue<byte[]>());
+            mockGraph.Verify(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)), Times.Once);
+            mockNextNode.Verify(x => x.Execute(
+                It.Is<IGraph>(x => x == mockGraph.Object),
+                It.Is<CancellationToken>(x => x == cancellationTokenSource.Token)), Times.Once);
+        }
+    }
+}
