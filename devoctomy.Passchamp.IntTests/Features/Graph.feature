7a00d06b47a6d2a8bbb9d806c70ab6e157088bf6
Nick Pateman <nick@devoctomy.com>
2021-06-14 20:46:40 +0100

    Started creating specflow integration test to create and execute an entire graph.

---
+++ devoctomy.Passchamp.IntTests/Features/Graph.feature
@@ -0,0 +1,20 @@
+﻿Feature: Graph
+
+Method of building up many small actions into a larger, more complex task.
+
+Scenario: Encrypt string using a password
+	Given A new dictionary of nodes
+	And RandomByteGeneratorNode named <SaltGeneratorNodeName> with a length of <SaltLength> and NextKey of <IvGeneratorNodeName>
+	And RandomByteGeneratorNode named <IvGeneratorNodeName> with a length of <IvLength>  and NextKey of <DeriveKeyNodeName>
+	And DeriveKeyFromPasswordNode named <DeriveKeyNodeName> with a password of <Password> and key length of <KeyLength> and NextKey of <Utf8EncoderNodeName>
+	And Utf8EncoderNode named <Utf8EncoderNodeName> and NextKey of <EncryptNodeName>
+	And EncryptNode named <EncryptNodeName> and NextKey of <JoinerNodeName>
+	And ArrayJoinerNode named <JoinerNodeName>
+	And A new graph with a start node named <SaltGeneratorNodeName>
+	When Execute graph
+	Then Something
+
+	Examples:
+
+	| SaltGeneratorNodeName | SaltLength | IvGeneratorNodeName | IvLength | DeriveKeyNodeName | Password | KeyLength | Utf8EncoderNodeName | EncryptNodeName | JoinerNodeName |
+	| saltgenerator         | 16         | ivgenerator         | 16       | derivekey         | 123      | 32        | encoder             | encrypt         | joiner         |
---
+++ devoctomy.Passchamp.IntTests/Steps/GraphStepDefinitions.cs
@@ -0,0 +1,128 @@
+﻿using devoctomy.Passchamp.Core.Graph;
+using devoctomy.Passchamp.Core.Graph.Cryptography;
+using devoctomy.Passchamp.Core.Graph.Data;
+using devoctomy.Passchamp.Core.Graph.Text;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace devoctomy.Passchamp.IntTests.Steps
+{
+    [Binding]
+    public sealed class GraphStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public GraphStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"A new dictionary of nodes")]
+        public void GivenANewDictionaryOfNodes()
+        {
+            var nodes = new Dictionary<string, INode>();
+            _scenarioContext.Set(nodes, "Nodes");
+        }
+
+        [Given(@"A new graph with a start node named (.*)")]
+        public void GivenANewGraphWithAStartNodeNamed(string startNode)
+        {
+            var nodes = _scenarioContext.Get<Dictionary<string, INode>>("Nodes");
+            _scenarioContext.Set(new Graph(nodes, startNode), "Graph");
+        }
+
+        [Given(@"RandomByteGeneratorNode named (.*) with a length of (.*) and NextKey of (.*)")]
+        public void GivenRandomByteGeneratorNodeNamedWithALengthOf(
+            string name,
+            int length,
+            string nextKey)
+        {
+            var nodes = _scenarioContext.Get<Dictionary<string, INode>>("Nodes");
+
+            var node = new RandomByteArrayGeneratorNode
+            {
+                Length = new DataPin(length),
+                NextKey = nextKey,
+            };
+
+            nodes.Add(name, node);
+        }
+
+        [Given(@"DeriveKeyFromPasswordNode named (.*) with a password of (.*) and key length of (.*) and NextKey of (.*)")]
+        public void GivenDeriveKeyFromPasswordNodeNamedWithAPasswordOfAndKeyLengthOf(
+            string name,
+            string password,
+            int keyLength,
+            string nextKey)
+        {
+            var nodes = _scenarioContext.Get<Dictionary<string, INode>>("Nodes");
+
+            var node = new DeriveKeyFromPasswordNode
+            {
+                Password = new DataPin(password),
+                KeyLength = new DataPin(keyLength),
+                NextKey = nextKey,
+            };
+
+            nodes.Add(name, node);
+        }
+
+        [Given(@"Utf8EncoderNode named (.*) and NextKey of (.*)")]
+        public void GivenUtfEncoderNodeNamed(
+            string name,
+            string nextKey)
+        {
+            var nodes = _scenarioContext.Get<Dictionary<string, INode>>("Nodes");
+
+            var node = new Utf8EncoderNode
+            {
+                PlainText = new DataPin("Hello World!"),
+                NextKey = nextKey,
+            };
+
+            nodes.Add(name, node);
+        }
+
+        [Given(@"EncryptNode named (.*) and NextKey of (.*)")]
+        public void GivenEncryptNodeNamedAndNextKeyOf(
+            string name,
+            string nextKey)
+        {
+            var nodes = _scenarioContext.Get<Dictionary<string, INode>>("Nodes");
+
+            var node = new EncryptNode
+            {
+                PlainTextBytes = ((Utf8EncoderNode)nodes["encoder"]).EncodedBytes,
+                NextKey = nextKey,
+            };
+
+            nodes.Add(name, node);
+        }
+
+        [Given(@"ArrayJoinerNode named (.*)")]
+        public void GivenArrayJoinerNodeNamed(string name)
+        {
+            var nodes = _scenarioContext.Get<Dictionary<string, INode>>("Nodes");
+
+            var node = new ArrayJoinerNode
+            {
+                Part1 = null,
+                Part2 = null,
+                Part3 = null,
+            };
+
+            nodes.Add(name, node);
+        }
+
+        [When(@"Execute graph")]
+        public void WhenExecuteGraph()
+        {
+
+        }
+
+        [Then(@"Something")]
+        public void ThenSomething()
+        {
+        }
+    }
+}
