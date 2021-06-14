4eb85c7855fbcbe86d9283f83deacc0d35ac7127
Nick Pateman <nick@devoctomy.com>
2021-06-14 18:50:36 +0100

    More test coverage.

---
+++ devoctomy.Passchamp.Core.UnitTests/Graph/Data/ArrayJoinerNodeTests.cs
@@ -0,0 +1,46 @@
+﻿using devoctomy.Passchamp.Core.Graph;
+using devoctomy.Passchamp.Core.Graph.Data;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace devoctomy.Passchamp.Core.UnitTests.Graph.Data
+{
+    public class ArrayJoinerNodeTests
+    {
+        [Fact]
+        public async Task GivenPart1_AndPart2_AndPart3_AndPart4_WhenExecute_ThenPartsJoined_AndNextExecuted()
+        {
+            // Arrange
+            var expectedJoinedArray = new byte[] { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4 };
+            var sut = new ArrayJoinerNode
+            {
+                Part1 = new DataPin(new byte[] { 1, 1, 1, 1 }),
+                Part2 = new DataPin(new byte[] { 2, 2, 2, 2 }),
+                Part3 = new DataPin(new byte[] { 3, 3, 3, 3 }),
+                Part4 = new DataPin(new byte[] { 4, 4, 4, 4 }),
+                NextKey = "hello"
+            };
+            var mockGraph = new Mock<IGraph>();
+            var mockNextNode = new Mock<INode>();
+            mockGraph.Setup(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)))
+                .Returns(mockNextNode.Object);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            await sut.Execute(
+                mockGraph.Object,
+                cancellationTokenSource.Token);
+
+            // Assert
+            Assert.Equal(expectedJoinedArray, sut.JoinedOutput.GetValue<byte[]>());
+            mockGraph.Verify(x => x.GetNode<INode>(
+                It.Is<string>(x => x == sut.NextKey)), Times.Once);
+            mockNextNode.Verify(x => x.Execute(
+                It.Is<IGraph>(x => x == mockGraph.Object),
+                It.Is<CancellationToken>(x => x == cancellationTokenSource.Token)), Times.Once);
+        }
+    }
+}
---
+++ devoctomy.Passchamp.Core.UnitTests/Graph/Data/ArrayLocationTests.cs
@@ -0,0 +1,31 @@
+﻿using devoctomy.Passchamp.Core.Graph.Data;
+using Xunit;
+
+namespace devoctomy.Passchamp.Core.UnitTests.Graph.Data
+{
+    public class ArrayLocationTests
+    {
+        [Theory]
+        [InlineData(Offset.Absolute, 10, 100, 10)]
+        [InlineData(Offset.Absolute, 0, 100, 0)]
+        [InlineData(Offset.FromEnd, 10, 100, 90)]
+        [InlineData(Offset.FromEnd, 0, 100, 100)]
+        public void GivenOffset_AndCount_AndDataLength_WhenGetIndex_ThenExpectedIndexReturned(
+            Offset offset,
+            int count,
+            int dataLength,
+            int expectedIndex)
+        {
+            // Arrange
+            var sut = new ArrayLocation(
+                offset,
+                count);
+
+            // Act
+            var result = sut.GetIndex(dataLength);
+
+            // Assert
+            Assert.Equal(expectedIndex, result);
+        }
+    }
+}
