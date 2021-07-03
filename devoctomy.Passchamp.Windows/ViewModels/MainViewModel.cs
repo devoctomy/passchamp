3977822c3e4f95bb717eb3365cfe1981c7a65c81
Nick Pateman <nick@devoctomy.com>
2021-07-03 17:22:17 +0100

    More additions.

--- devoctomy.Passchamp.Windows/ViewModels/MainViewModel.cs
+++ devoctomy.Passchamp.Windows/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using devoctomy.Passchamp.Core.Graph.Services;
-using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Windows.Input;
 
 namespace devoctomy.Passchamp.Windows.ViewModels
 {
@@ -15,9 +17,22 @@ namespace devoctomy.Passchamp.Windows.ViewModels
             private set => SetProperty(ref _testValue, value);
         }
 
+        public ICommand Test { get; }
+
         public MainViewModel(IGraphLoaderService graphLoaderService)
         {
             _graphLoaderService = graphLoaderService;
+            Test = new RelayCommand(DoStuff, DoStuffCanExecute);
+        }
+
+        private bool DoStuffCanExecute()
+        {
+            return true;
+        }
+
+        private void DoStuff()
+        {
+            TestValue = Guid.NewGuid().ToString();
         }
     }
 }
