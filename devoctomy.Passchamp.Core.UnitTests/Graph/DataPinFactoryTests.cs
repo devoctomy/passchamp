71f6aa8b745f8d23f022113910c259cc1dd0855f
Nick Pateman <nick@devoctomy.com>
2021-07-25 15:33:30 +0100

    Added tests for datapinfactory

---
+++ devoctomy.Passchamp.Core.UnitTests/Graph/DataPinFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using devoctomy.Passchamp.Core.Graph;
+using System;
+using Xunit;
+
+namespace devoctomy.Passchamp.Core.UnitTests.Graph
+{
+    public class DataPinFactoryTests
+    {
+        [Theory]
+        [InlineData(0, typeof(DataPin<int>))]
+        [InlineData("Hello", typeof(DataPin<string>))]
+        [InlineData(false, typeof(DataPin<bool>))]
+        [InlineData(new[]{ (byte)0, (byte)1, (byte)2 }, typeof(DataPin<byte[]>))]
+        public void GivenName_AndValue_WhenCreate_ThentPinOfCorrectTypeCreated(
+            object value,
+            Type dataPinType)
+        {
+            //Arrange
+
+            //Act
+            var result = DataPinFactory.Instance.Create(
+                "Test",
+                value);
+
+            //Assert
+            Assert.Equal(dataPinType, result.GetType());
+        }
+    }
+}
