376c071e6a454532fa5979ffc155e84a25fe1d57
Nick Pateman <nick@devoctomy.com>
2021-07-25 17:44:02 +0100

    More test coverage.

---
+++ devoctomy.Passchamp.Core.UnitTests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using devoctomy.Passchamp.Core.Extensions;
+using devoctomy.Passchamp.Core.Graph.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace devoctomy.Passchamp.Core.UnitTests.Extensions
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void GivenServiceCollection_WhenAddPasschampCoreServices_ThenServicesAdded()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            serviceCollection.AddPasschampCoreServices();
+
+            // Assert
+            var provider = serviceCollection.BuildServiceProvider();
+            Assert.NotNull(provider.GetService<IGraphLoaderService>());
+            Assert.NotNull(provider.GetService<INodesJsonParserService>());
+            Assert.NotNull(provider.GetService<IPinsJsonParserService>());
+        }
+    }
+}
