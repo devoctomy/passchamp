9f0bb43d72ecf2323742705084001bb7743fb583
Nick Pateman <nick@devoctomy.com>
2021-07-25 16:20:52 +0100

    More test coverage.

--- devoctomy.Passchamp.Core.UnitTests/Graph/DataPinFactoryTests.cs
+++ devoctomy.Passchamp.Core.UnitTests/Graph/DataPinFactoryTests.cs
@@ -1,5 +1,7 @@
 ﻿using devoctomy.Passchamp.Core.Graph;
+using devoctomy.Passchamp.Core.Graph.Data;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace devoctomy.Passchamp.Core.UnitTests.Graph
@@ -24,6 +26,26 @@ namespace devoctomy.Passchamp.Core.UnitTests.Graph
 
             //Assert
             Assert.Equal(dataPinType, result.GetType());
+        }
+
+        [Theory]
+        [InlineData(typeof(DataParserSection))]
+        public void GivenName_AndGenericListValue_WhenCreate_ThentPinOfCorrectTypeCreated(Type listType)
+        {
+            //Arrange
+            var genericListType = typeof(List<>);
+            var constructedListType = genericListType.MakeGenericType(listType);
+            var value = Activator.CreateInstance(constructedListType);
+
+            //Act
+            var result = DataPinFactory.Instance.Create(
+                "Test",
+                value);
+
+            //Assert
+            var genericDataPinType = typeof(DataPin<>);
+            var constructedDataPinType = genericDataPinType.MakeGenericType(constructedListType);
+            Assert.Equal(constructedDataPinType, result.GetType());
         }
     }
 }
