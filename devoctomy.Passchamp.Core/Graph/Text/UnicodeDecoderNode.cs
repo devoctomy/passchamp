5edbd50f06ee9531dfa56d894a62af7cfc313915
Nick Pateman <nick@devoctomy.com>
2021-08-30 12:12:09 +0100

    Legacy cache graph now decrypting old vaults successfully. Created new unicode nodes.

---
+++ devoctomy.Passchamp.Core/Graph/Text/UnicodeDecoderNode.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace devoctomy.Passchamp.Core.Graph.Text
+{
+    public class UnicodeDecoderNode : NodeBase
+    {
+        [NodeInputPin(ValueType = typeof(byte[]), DefaultValue = default(byte[]))]
+        public IDataPin<byte[]> EncodedBytes
+        {
+            get
+            {
+                return GetInput<byte[]>("EncodedBytes");
+            }
+            set
+            {
+                Input["EncodedBytes"] = value;
+            }
+        }
+
+        [NodeOutputPin(ValueType = typeof(string), DefaultValue = "")]
+        public IDataPin<string> PlainText
+        {
+            get
+            {
+                return GetOutput<string>("PlainText");
+            }
+        }
+
+        protected override Task DoExecuteAsync(
+            IGraph graph,
+            CancellationToken cancellationToken)
+        {
+            PlainText.Value = System.Text.Encoding.Unicode.GetString(EncodedBytes.Value);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ devoctomy.Passchamp.Core/Graph/Text/UnicodeEncoderNode.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace devoctomy.Passchamp.Core.Graph.Text
+{
+    public class UnicodeEncoderNode : NodeBase
+    {
+        [NodeInputPin(ValueType = typeof(string), DefaultValue = "")]
+        public IDataPin<string> PlainText
+        {
+            get
+            {
+                return GetInput<string>("PlainText");
+            }
+            set
+            {
+                Input["PlainText"] = value;
+            }
+        }
+
+        [NodeOutputPin(ValueType = typeof(byte[]))]
+        public IDataPin<byte[]> EncodedBytes
+        {
+            get
+            {
+                return GetOutput<byte[]>("EncodedBytes");
+            }
+        }
+
+        protected override Task DoExecuteAsync(
+            IGraph graph,
+            CancellationToken cancellationToken)
+        {
+            EncodedBytes.Value = System.Text.Encoding.UTF8.GetBytes(PlainText.Value);
+            return Task.CompletedTask;
+        }
+    }
+}
