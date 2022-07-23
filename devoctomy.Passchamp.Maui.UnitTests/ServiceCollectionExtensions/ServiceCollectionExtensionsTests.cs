7854e6fcc97a206e1722c6a89d2ae276f1197d83
Nick Pateman <nick@npsoftware.co.uk>
2022-07-23 14:27:33 +0100

    More test coverage.

---
+++ devoctomy.Passchamp.Maui.UnitTests/ServiceCollectionExtensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using devoctomy.Passchamp.Core.Data;
+using devoctomy.Passchamp.Maui.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Storage;
+using Moq;
+
+namespace devoctomy.Passchamp.Maui.UnitTests.ServiceCollectionExtensions
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void GivenServiceCollection_WhenAddPasschampMauiServices_ThenServicesAdded()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<ISecureStorage>(Mock.Of<ISecureStorage>());
+
+            // Act
+            serviceCollection.AddPasschampMauiServices();
+
+            // Assert
+            var provider = serviceCollection.BuildServiceProvider();
+            Assert.NotNull(provider.GetService<ISecureSettingStorageService>());
+        }
+    }
+}
