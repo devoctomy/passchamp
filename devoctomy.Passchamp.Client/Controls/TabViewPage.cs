ee4f1e67c88c6be3202d37512ac359152a7ff9ba
Nick Pateman <nick@npsoftware.co.uk>
2024-02-03 16:25:43 +0000

    fix: fixed issue with tab styles not applying in datatrigger, rectangle background not working properly, presumed MAUI bug

--- devoctomy.Passchamp.Client/Controls/TabViewPage.cs
+++ devoctomy.Passchamp.Client/Controls/TabViewPage.cs
@@ -1,78 +1,24 @@
-﻿using devoctomy.Passchamp.Client.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace devoctomy.Passchamp.Client.Controls;
 
-public partial class TabViewPage : BindableObject
+public partial class TabViewPage : ObservableObject
 {
-    public static readonly BindableProperty IsSelectedProperty = BindableProperty.Create(
-        nameof(IsSelected),
-        typeof(bool),
-        typeof(TabViewPage),
-        false);
+    [ObservableProperty]
+    private bool isSelected;
 
-    public static readonly BindableProperty TitleProperty = BindableProperty.Create(
-        nameof(Title),
-        typeof(string),
-        typeof(TabViewPage),
-        string.Empty);
+    [ObservableProperty]
+    private string title;
 
-    public static readonly BindableProperty WidthProperty = BindableProperty.Create(
-        nameof(Width),
-        typeof(double),
-        typeof(TabViewPage),
-        60d);
+    [ObservableProperty]
+    private double width;
 
-    public static readonly BindableProperty ContentProperty = BindableProperty.Create(
-        nameof(Content),
-        typeof(ContentView),
-        typeof(TabViewPage),
-        null);
+    [ObservableProperty]
+    private ContentView content;
 
-    public static readonly BindableProperty ContentTypeProperty = BindableProperty.Create(
-        nameof(ContentType),
-        typeof(Type),
-        typeof(TabViewPage),
-        null);
+    [ObservableProperty]
+    private Type contentType;
 
-    public static readonly BindableProperty ViewModelPropertyNameProperty = BindableProperty.Create(
-        nameof(ViewModelPropertyName),
-        typeof(string),
-        typeof(TabViewPage),
-        string.Empty);
-
-    public bool IsSelected
-    {
-        get => (bool)GetValue(IsSelectedProperty);
-        set => SetValue(IsSelectedProperty, value);
-    }
-
-    public string Title
-    {
-        get => (string)GetValue(TitleProperty);
-        set => SetValue(TitleProperty, value);
-    }
-
-    public double Width
-    {
-        get => (double)GetValue(WidthProperty);
-        set => SetValue(WidthProperty, value);
-    }
-
-    public ContentView Content
-    {
-        get => (ContentView)GetValue(ContentProperty);
-        set => SetValue(ContentProperty, value);
-    }
-
-    public Type ContentType
-    {
-        get => (Type)GetValue(ContentTypeProperty);
-        set => SetValue(ContentTypeProperty, value);
-    }
-
-    public string ViewModelPropertyName
-    {
-        get => (string)GetValue(ViewModelPropertyNameProperty);
-        set => SetValue(ViewModelPropertyNameProperty, value);
-    }
+    [ObservableProperty]
+    private string viewModelPropertyName;
 }
