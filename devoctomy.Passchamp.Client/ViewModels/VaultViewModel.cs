34291f5e733db95d115e0e14c5d9de2f33e234d7
Nick Pateman <nick@npsoftware.co.uk>
2024-03-03 17:32:17 +0000

    chore: Added some dummy menu options for vault page

--- devoctomy.Passchamp.Client/ViewModels/VaultViewModel.cs
+++ devoctomy.Passchamp.Client/ViewModels/VaultViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using devoctomy.Passchamp.Client.ViewModels.Base;
 using devoctomy.Passchamp.Maui.Services;
 
@@ -26,6 +27,41 @@ public partial class VaultViewModel : BaseAppShellPageViewModel
             Command = LockVaultCommand,
             IconImageSource = _themeAwareImageResourceService.Get("lock_wf")
         });
+
+        MenuItems.Add(new MenuItem
+        {
+            Text = "Create Credential",
+            Command = CreateCredentialCommand,
+            IconImageSource = null
+        });
+
+        MenuItems.Add(new MenuItem
+        {
+            Text = "Import From CSV",
+            Command = ImportFromCSVCommand,
+            IconImageSource = null
+        });
+
+        MenuItems.Add(new MenuItem
+        {
+            Text = "Export To CSV",
+            Command = ImportFromCSVCommand,
+            IconImageSource = null
+        });
+
+        MenuItems.Add(new MenuItem
+        {
+            Text = "Report",
+            Command = ReportCommand,
+            IconImageSource = null
+        });
+
+        MenuItems.Add(new MenuItem
+        {
+            Text = "Audit",
+            Command = AuditCommand,
+            IconImageSource = null
+        });
     }
 
     protected override async Task OnFirstAppearanceAsync()
@@ -37,5 +73,40 @@ public partial class VaultViewModel : BaseAppShellPageViewModel
     private async Task LockVault(object param)
     {
         await _shellNavigationService.GoToAsync("//Vaults");
+    }
+
+    [RelayCommand]
+    private async Task CreateCredential(object param)
+    {
+        // show credential creation page
+        await Task.Yield();
+    }
+
+    [RelayCommand]
+    private async Task ImportFromCSV(object param)
+    {
+        // show import page
+        await Task.Yield();
+    }
+
+    [RelayCommand]
+    private async Task ExportToSV(object param)
+    {
+        // show import page
+        await Task.Yield();
+    }
+
+    [RelayCommand]
+    private async Task Report(object param)
+    {
+        // show report page
+        await Task.Yield();
+    }
+
+    [RelayCommand]
+    private async Task Audit(object param)
+    {
+        // show audit page
+        await Task.Yield();
     }
 }
